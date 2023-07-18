3ad82fc248bc54c99a36899794c2cb026f75eff0
Victor Quinderé <victorquindere@gmail.com>
2023-07-18 17:51:48 -0300

    Still experimenting the Contratos. Trying to clean the code more.

---
+++ src/ContratoRenderBROKE.tsx
@@ -0,0 +1,16 @@
+// import React, { useState } from "react";
+// import InputFields from "./InputFields";
+// import Contratos from "./Contratos";
+
+// const ContratoRender: React.FC = () => {
+//   const [fields, setFields] = useState([]);
+
+//   return (
+//     <>
+//       <InputFields setFields={setFields} />
+//       <Contratos fields={fields} />
+//     </>
+//   );
+// };
+
+// export default ContratoRender;
--- src/Inputs.tsx
+++ src/Inputs.tsx
@@ -44,9 +44,61 @@ const Contratos: React.FC = () => {
   return (
     <div>
       {renderInputs()}
-      {/* conteúdo do contratos.tsx */}
-    </div>
+     </div>
   );
 };
 
 export default Contratos;
+
+// import React, { useState } from "react";
+
+// type InputFieldsProps = {
+//   setFields: React.Dispatch<React.SetStateAction<never[]>>;
+// };
+
+// const InputFields: React.FC<InputFieldsProps> = () => {
+//   const [fields, setFields] = useState([
+//     { label: "Nome do Proprietário", value: "" },
+//     { label: "Endereço do Proprietário", value: "" },
+//     { label: "Nome do Locatário", value: "" },
+//     { label: "Endereço do Locatário", value: "" },
+//     { label: "Endereço do Imóvel", value: "" },
+//     { label: "Descrição do Imóvel", value: "" },
+//     { label: "Data de Início", value: "" },
+//     { label: "Duração do Contrato", value: "" },
+//     { label: "Valor do Aluguel", value: "" },
+//     { label: "Meio de Pagamento", value: "" },
+//     { label: "Informações da Conta Bancária", value: "" },
+//     { label: "Valor da Caução", value: "" },
+//     { label: "Prazo de Devolução da Caução", value: "" },
+//     { label: "Prazo de Aviso Prévio", value: "" },
+//     { label: "País/Estado", value: "" },
+//     { label: "Data da Assinatura", value: "" },
+//   ]);
+
+//   const handleFieldChange = (
+//     index: number,
+//     event: React.ChangeEvent<HTMLInputElement>
+//   ) => {
+//     const updatedFields = [...fields];
+//     updatedFields[index].value = event.target.value;
+//     setFields(updatedFields);
+//   };
+
+//   const renderInputs = () => {
+//     return fields.map((field, index) => (
+//       <input
+//         key={index}
+//         type="text"
+//         value={field.value}
+//         onChange={(event) => handleFieldChange(index, event)}
+//         placeholder={field.label}
+//       />
+//     ));
+//   };
+
+//   return <>{renderInputs()}</>;
+// };
+
+// export default InputFields;
+
