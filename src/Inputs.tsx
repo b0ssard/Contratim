470312c32c00eabb27fbea0f7970c741be383246
Victor Quinderé <victorquindere@gmail.com>
2023-07-19 10:59:24 -0300

    Separated the contract fields in 3 files.

--- src/Inputs.tsx
+++ src/Inputs.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Inputs: React.FC<Record<string, never>> = () => {
-  const [fields, setFields] = useState([
-    { label: "Nome do Proprietário", value: "" },
-    { label: "Endereço do Proprietário", value: "" },
-    { label: "Nome do Locatário", value: "" },
-    { label: "Endereço do Locatário", value: "" },
-    { label: "Endereço do Imóvel", value: "" },
-    { label: "Descrição do Imóvel", value: "" },
-    { label: "Data de Início", value: "" },
-    { label: "Duração do Contrato", value: "" },
-    { label: "Valor do Aluguel", value: "" },
-    { label: "Meio de Pagamento", value: "" },
-    { label: "Informações da Conta Bancária", value: "" },
-    { label: "Valor da Caução", value: "" },
-    { label: "Prazo de Devolução da Caução", value: "" },
-    { label: "Prazo de Aviso Prévio", value: "" },
-    { label: "País/Estado", value: "" },
-    { label: "Data da Assinatura", value: "" },
-  ]);
-
-  const handleFieldChange = (
+interface ContractInputsProps {
+  fields: Array<{ label: string; value: string }>;
+  handleFieldChange: (
     index: number,
     event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const updatedFields = [...fields];
-    updatedFields[index].value = event.target.value;
-    setFields(updatedFields);
-  };
+  ) => void;
+}
 
+const ContractInputs: React.FC<ContractInputsProps> = ({
+  fields,
+  handleFieldChange,
+}) => {
   return (
     <>
       {fields.map((field, index) => (
@@ -44,7 +27,57 @@ const Inputs: React.FC<Record<string, never>> = () => {
   );
 };
 
-export default Inputs;
+export default ContractInputs;
+
+// import React, { useState } from "react";
+
+// const Inputs: React.FC<Record<string, never>> = () => {
+//   const [fields, setFields] = useState([
+//     { label: "Nome do Proprietário", value: "" },
+//     { label: "Endereço do Proprietário", value: "" },
+//     { label: "Nome do Locatário", value: "" },
+//     { label: "Endereço do Locatário", value: "" },
+//     { label: "Endereço do Imóvel", value: "" },
+//     { label: "Descrição do Imóvel", value: "" },
+//     { label: "Data de Início", value: "" },
+//     { label: "Duração do Contrato", value: "" },
+//     { label: "Valor do Aluguel", value: "" },
+//     { label: "Meio de Pagamento", value: "" },
+//     { label: "Informações da Conta Bancária", value: "" },
+//     { label: "Valor da Caução", value: "" },
+//     { label: "Prazo de Devolução da Caução", value: "" },
+//     { label: "Prazo de Aviso Prévio", value: "" },
+//     { label: "País/Estado", value: "" },
+//     { label: "Data da Assinatura", value: "" },
+//   ]);
+
+//   const handleFieldChange = (
+//     index: number,
+//     event: React.ChangeEvent<HTMLInputElement>
+//   ) => {
+//     const updatedFields = [...fields];
+//     updatedFields[index].value = event.target.value;
+//     setFields(updatedFields);
+//   };
+
+//   return (
+//     <>
+//       {fields.map((field, index) => (
+//         <input
+//           key={index}
+//           type="text"
+//           value={field.value}
+//           onChange={(event) => handleFieldChange(index, event)}
+//           placeholder={field.label}
+//         />
+//       ))}
+//     </>
+//   );
+// };
+
+// export default Inputs;
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 
 // import React, { useState } from "react";
 
