78f6c4bd004dccd39dbd5976bfa5826621fbd22d
Victor Quinderé <victorquindere@gmail.com>
2023-10-23 13:47:22 -0300

    Refatorado o MyContracts.

--- src/MyContracts.tsx
+++ src/MyContracts.tsx
@@ -9,7 +9,7 @@ import {
   DocumentData,
   QuerySnapshot,
   updateDoc,
-  doc
+  doc,
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, Auth, User } from "firebase/auth";
 import { db } from "./firebase-config";
@@ -29,22 +29,6 @@ const MyContracts: React.FC = () => {
   const [editedContract, setEditedContract] = useState<ContractData | null>(
     null
   );
-
-  const updateContract = async () => {
-    if (editedContract) {
-      try {
-        const docRef = doc(db, "filledContracts", editedContract.id);
-        await updateDoc(docRef, {
-          data: editedContract.data,
-        });
-        console.log("Dados atualizados com sucesso!");
-      } catch (error) {
-        console.error("Erro ao atualizar dados: ", error);
-      }
-    }
-  };
-
-  
 
   const clearInputData = () => {
     const inputs = document.querySelectorAll(
@@ -68,53 +52,96 @@ const MyContracts: React.FC = () => {
     }
   };
 
+  const updateContract = async () => {
+    if (editedContract) {
+      try {
+        const docRef = doc(db, "filledContracts", editedContract.id);
+        await updateDoc(docRef, { data: editedContract.data });
+        console.log("Dados atualizados com sucesso!");
+      } catch (error) {
+        console.error("Erro ao atualizar dados: ", error);
+      }
+    }
+  };
+
   useEffect(() => {
     const auth: Auth = getAuth();
-
-    const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user: User | null) => {
       if (!user) {
         setLoading(false);
         setError("Você não está logado.");
         return;
       }
-
       setUserEmail(user.email);
 
-      const fetchUserContracts = async () => {
-        try {
-          const filledContractsCollection = collection(db, "filledContracts");
-          const q = query(
-            filledContractsCollection,
-            where("userEmail", "==", user.email)
-          );
-          const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
-          const contractsData: ContractData[] = querySnapshot.docs.map(
-            (doc) => {
-              const data = doc.data();
-              return {
-                id: doc.id,
-                contractType: data.contractType,
-                status: data.status,
-                data: data.data,
-              };
-            }
-          );
+      try {
+        const filledContractsCollection = collection(db, "filledContracts");
+        const q = query(
+          filledContractsCollection,
+          where("userEmail", "==", user.email)
+        );
+        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
+        const contractsData: ContractData[] = querySnapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            contractType: data.contractType,
+            status: data.status,
+            data: data.data,
+          };
+        });
 
-          setUserContracts(contractsData);
-          setLoading(false);
-        } catch (error) {
-          setError(
-            "Erro ao buscar contratos do usuário: " + (error as Error).message
-          );
-          setLoading(false);
-        }
-      };
-
-      fetchUserContracts();
+        setUserContracts(contractsData);
+        setLoading(false);
+      } catch (error) {
+        setError(
+          "Erro ao buscar contratos do usuário: " + (error as Error).message
+        );
+        setLoading(false);
+      }
     });
 
     return () => unsubscribe();
   }, []);
+
+  const renderContractDetails = () => {
+    if (!editedContract) return null;
+
+    return (
+      <div>
+        <Heading fontSize={["2xl"]} mt={5}>
+          Detalhes do Contrato Selecionado
+        </Heading>
+        <Text>
+          <strong>Tipo de Contrato:</strong> {editedContract.contractType}
+          <br />
+          <strong>Status:</strong> {editedContract.status}
+          <br />
+          <strong>ID do Contrato:</strong> {editedContract.id}
+          <br />
+          <strong>Detalhes:</strong>
+          <br />
+          {Object.entries(editedContract.data).map(([key, value]) => (
+            <React.Fragment key={key}>
+              <strong>{key}:</strong>{" "}
+              <Input
+                placeholder={value.trim() === "" ? "Não preenchido" : ""}
+                size="md"
+                borderRadius="md"
+                _focus={{ borderColor: "blue.500" }}
+                _placeholder={{ color: "gray.500" }}
+                type="text"
+                value={value}
+                onChange={(e) => handleInputChange(key, e.target.value)}
+              />
+              <br />
+            </React.Fragment>
+          ))}
+          <Button onClick={updateContract}>Atualizar</Button>
+        </Text>
+      </div>
+    );
+  };
 
   if (loading) {
     return <div>Carregando contratos...</div>;
@@ -147,42 +174,7 @@ const MyContracts: React.FC = () => {
               </ListItem>
             ))}
           </List>
-
-          {editedContract && (
-            <div>
-              <Heading fontSize={["2xl"]} mt={5}>
-                Detalhes do Contrato Selecionado
-              </Heading>
-              <Text>
-                <strong>Tipo de Contrato:</strong> {editedContract.contractType}
-                <br />
-                <strong>Status:</strong> {editedContract.status}
-                <br />
-                <strong>ID do Contrato:</strong> {editedContract.id}
-                <br />
-                <strong>Detalhes:</strong>
-                <br />
-                {Object.entries(editedContract.data).map(([key, value]) => (
-                  <React.Fragment key={key}>
-                    <strong>{key}:</strong>{" "}
-                    <Input
-                      placeholder={value.trim() === "" ? "Não preenchido" : ""}
-                      size="md"
-                      borderRadius="md"
-                      _focus={{ borderColor: "blue.500" }}
-                      _placeholder={{ color: "gray.500" }}
-                      type="text"
-                      value={value}
-                      onChange={(e) => handleInputChange(key, e.target.value)}
-                    />
-                    <br />
-                  </React.Fragment>
-                ))}
-                <Button onClick={updateContract}>Update</Button>
-                <br />
-              </Text>
-            </div>
-          )}
+          {renderContractDetails()}
         </div>
       )}
     </Box>
